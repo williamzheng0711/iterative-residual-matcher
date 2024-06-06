0f8e767129bfc159a060dc7b9c3076128a1db7a7
Weijia <zhengwj.william@gmail.com>
2024-06-06 19:38:39 +0800

    Made algorithm more efficient

--- knownSupp.m
+++ knownSupp.m
@@ -1,12 +1,12 @@
 % Generate the matrix
 m = 2^8;  % =256
-N = 10000; %
+N = 65536; %
 P = 15;   % P stands for power
 H = sqrt(P) * 1/sqrt(m)*randn(m, N);
 
 % Generate the column vector of channels, each entry is exponential distribution with mean 5
-Ka = 10; 
-x_init = exprnd(0.3, [Ka, 1]);
+Ka = 50; 
+x_init = exprnd(15, [Ka, 1]);
 x_init = sort(x_init, "descend");  % better channels get decoded first
 
 % Ranomly choose Ka codewords from N
@@ -29,41 +29,42 @@ y_observe = y_true + z;
 x = x_init; 
 y = y_observe; 
 
-numCandidates = 5*Ka; 
-candidates = []; 
-dp = @(v) v'*y;
-dp_results = arrayfun(@(t) dp(H(:,t)), 1:size(H, 2));
-[~, guessActive] = maxk(dp_results, numCandidates);
-disp(guessActive)
-disp(length(setdiff(chosenNums, guessActive))); 
-
 RowCollects = []; 
-ColCollects = [];
 
 for j = 1:Ka
     if j < Ka
-        cvx_begin 
-            variable P(N,Ka-(j-1))
+        numCandidates = 5*Ka; 
+        candidates = []; 
+        dp = @(v) v'*y;
+        dp_results = arrayfun(@(t) dp(H(:,t)), 1:size(H, 2));
+        if j > 1
+            dp_results(RowCollects) = -Inf;
+        end
+        [~, guessActive] = maxk(dp_results, numCandidates);
+        % disp(guessActive)
+        % disp(length(setdiff(chosenNums, guessActive))); 
+
+        cvx_begin quiet
+            variable P(numCandidates, Ka-(j-1))
             fprintf('Now j is: %d.\n', j);
     
             P >= 0;
-            ones(1,N)*P == ones(1, Ka-(j-1)); 
-            P*ones(Ka-(j-1),1) <= ones(N,1);
+            ones(1,numCandidates)*P == ones(1, Ka-(j-1)); 
+            P*ones(Ka-(j-1),1) <= ones(numCandidates,1);
             
-            if nnz(RowCollects)>0
-                P(RowCollects,:) == 0;
-            end
+%             if nnz(RowCollects)>0
+%                 P(RowCollects,:) == 0;
+%             end
         
             % Objective function
-            minimize(norm(y-H*P*x, 2))
+            minimize(norm(y-H(:,guessActive)*P*x, 2))
         cvx_end
         
         [value, index] = max(P(:));
-        [row, col] = ind2sub(size(P), index);
-        ColCollects(j) = col; 
-        RowCollects(j) = row; 
-        y = y - H(:,RowCollects(j))*x(ColCollects(j));
-        x(ColCollects(j)) = [];
+        [row, col] = ind2sub(size(P), index); 
+        RowCollects(j) = guessActive(row); 
+        y = y - H(:,RowCollects(j))*x(col);
+        x(col) = [];
     end 
 
     if j == Ka % If 
