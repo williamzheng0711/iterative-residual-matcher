9de62383fcce95b68ce0f2a20f1b4d1223fed3d1
Weijia <zhengwj.william@gmail.com>
2024-06-24 22:08:30 +0800

    updated the file

--- cvx_optm.m
+++ cvx_optm.m
@@ -1,18 +1,17 @@
 % Generate the matrix
 m = 256;  % =2^8
-N = 8192; % =2^13
+N = 2048; % =2^11
 P = 10;   % P stands for power
 H = sqrt(P) * 1/sqrt(m)*randn(m, N);
 
 % Generate the column vector of channels, each entry is exponential distribution with mean 5
-Ka = 100; 
-x_init = exprnd(15, [Ka, 1]);
+Ka = 70; 
+x_init = exprnd(15, [Ka, 1]); 
 x_init = sort(x_init, "descend");  % better channels get decoded first, in general
 
 % Ranomly choose Ka codewords from N
 % chosenNums = randperm(N, Ka);
 chosenNums = 1:Ka; 
-% disp(chosenNums); 
 disp(x_init); 
 
 % Generate the true superpositioned signal
@@ -22,8 +21,8 @@ for i = 1:Ka
 end
 
 % Additive noise (variance is 1, normalized)
-% z = randn(m, 1);
-z = zeros(m,1); 
+z = randn(m, 1);
+% z = zeros(m,1); 
 
 % Produce the "observable" y: 
 y_observe = y_true + z; 
@@ -34,12 +33,12 @@ RowCollects = [];
 cdwdToChannel = []; 
 
 u = zeros(N,1);
-u(1:N-Ka,1) = 1;
+u(randperm(N, N-Ka),1) = 1;
 
 objval = 100; 
 
 j = 0;
-while objval > 0.0001
+while objval > 0.0001 || isnan(objval)
     if j > 50
         break;
     end
@@ -55,23 +54,36 @@ while objval > 0.0001
         P*ones(Ka,1) <= ones(N,1);
     
         % Objective function
-        minimize(100*norm(y-H*P*x, 2) + u'*P*ones(Ka,1)) 
+        minimize( 1000*Ka/norm(y,2)*norm(y-H*P*x, 2) + u'*P*ones(Ka,1)) 
     cvx_end
 
     objval = cvx_optval;
     
     probs = P*ones(Ka,1); 
     [probs, idx] = sort(probs, "ascend"); 
+
+    u_temp = u; 
     u = zeros(N,1);
-    u(idx(1:N-Ka), 1) = 1;
+    u(idx(1:N-Ka), 1) = 1; 
+    disp(find(u_temp' ==0)); 
+    disp(find(u' ==0)); 
     j = j + 1;
+    
+    if prod(find(u'==0)==find(u_temp'==0)) == 1
+        break;
+    end
+   
 end
 
 
 % Display the indices
+fprintf(" /// /// /// /// /// /// ///")
+fprintf(" /// /// /// /// /// /// ///")
+fprintf(" /// /// /// /// /// /// ///")
+
 fprintf('These are the estimated active messages')
 disp( find(u'==0) );
 fprintf('These are the true active messages     ')
 disp(chosenNums);
 
-disp(length(setdiff(RowCollects, chosenNums))); 
+disp(length(setdiff(find(u'==0), chosenNums))); 
